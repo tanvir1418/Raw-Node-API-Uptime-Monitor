f2534d1ebd1d293e12ee67a464534c98f847c092
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2021-06-17 13:40:38 +0600

    Raw Node API Project (File CRUD System) - Part 2

--- index.js
+++ index.js
@@ -8,20 +8,35 @@
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
+// testing file system
+// // File Handling: Create File
+// data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
+//     console.log('error was', err);
+// });
+// // File Handling: Read File
+// data.read('test', 'newFile', (err, result) => {
+//     console.log(err, result);
+// });
+// // File Handling: Update File
+// data.update('test', 'newFile', { name: 'England', language: 'English' }, (err) => {
+//     console.log(err);
+// });
+// // File Handling: Delete File
+// data.delete('test', 'newFile', (err) => {
+//     console.log(err);
+// });
 
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`Listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`Listening to port ${environment.port}`);
     });
 };
 
