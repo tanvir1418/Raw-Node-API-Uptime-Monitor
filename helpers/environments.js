2e0278df15f7d02e361ae8ec429b9add3dab992a
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2021-06-20 20:35:30 +0600

    Checks GET, POST, PUT, DELETE Done - Part 5

--- helpers/environments.js
+++ helpers/environments.js
@@ -6,12 +6,14 @@ environments.staging = {
     port: 3000,
     envName: 'staging',
     secretKey: 'dafdhalhfladjklljd',
+    maxChecks: 5,
 };
 
 environments.production = {
     port: 5000,
     envName: 'production',
     secretKey: 'ldafhncnvafgljejdl',
+    maxChecks: 5,
 };
 
 // determine which environment was passed
